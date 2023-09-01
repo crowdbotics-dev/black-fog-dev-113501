1a7cd1b77dab3e84fd5a55445368cba7574eabc8
Crowdbotics <engineering@crowdbotics.com>
2023-09-01 15:10:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "black-fog-dev-113501",
+  url: "https://black-fog-dev-113501.botics.co",
+  api: "https://black-fog-dev-113501.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
